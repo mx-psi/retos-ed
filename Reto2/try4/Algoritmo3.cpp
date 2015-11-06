b6fecf7f0aed90f7730258f5b5d12f5af45be726
pbaeyens <pbaeyens@users.noreply.github.com>
2015-11-06 16:57:46 +0100

    3, no 4

---
+++ Reto2/try4/Algoritmo3.cpp
@@ -0,0 +1,96 @@
+#include "pila.h"
+#include "vector.h"
+#include <ctime>
+#include <algorithm>
+using namespace std;
+
+bool Algo3(int obj, Vector& nums, Pila& p, int& m_a){
+
+  // Caso base: 2 elementos
+  if(nums.size() == 2){
+    for (int op = 0; op < 4; op++) {
+      int res = Opera(nums[0], nums[1], op);
+      if(res == 0)
+        continue;
+
+      if(abs(res-obj) < abs(m_a-obj)){
+        m_a = res;
+        if(obj == m_a){
+          p.push({nums[0], nums[1], op});
+          return true;
+        }
+      }
+    }
+   return false;
+  }
+
+  // Caso recursivo: más de 2
+  for (int op = 0; op < 4; op++) {
+    for (int i = nums.inicial(op); i < nums.size(); i+=nums.avanza(i)){
+      int nums_i = nums[i];
+      for (int j = i+1; j < nums.size(); j+=nums.avanza(j)){
+        int nums_j = nums[j];
+        int res = Opera(nums_i, nums_j, op);
+        if(res == 0 || res == nums_i || res == nums_j) continue;
+
+        p.push({nums_i, nums_j, op});
+        nums.borra(j);
+        nums.borra(i);
+        int pos = nums.inserta(res);
+
+        if(abs(res-obj) < abs(m_a-obj)){
+          m_a = res;
+          if(obj == m_a)
+            return true;
+        }
+        if(Algo3(obj,nums,p,m_a))
+          return true;
+
+        p.pop();
+        nums.borra(pos);
+        nums.inserta(nums_j);
+        nums.inserta(nums_i);
+      }
+    }
+  }
+  return false;
+}
+
+void Cifras(int obj, int iniciales[6]) {
+  Pila p;
+  int m_a = 0;
+  sort(iniciales, iniciales+6);
+  Vector nums(iniciales);
+  bool usados[6];
+
+  cout << nums << "⟶ " << obj << endl;
+  Algo3(obj,nums,p,m_a);
+
+  if(obj != m_a){
+    nums.reinicia(iniciales);
+    p.clear();
+    obj = m_a;
+    m_a = 0;
+    Algo3(obj,nums,p,m_a);
+  }
+
+  cout << "Obtenida: " << m_a << endl;
+  p.Imprime(iniciales, usados);
+}
+
+int main(){
+  int posibles[14] = {1,2,3,4,5,6,7,8,9,10,25,50,75,100};
+
+  srand(time(0));
+  int obj = (rand()%900) + 100;
+  int iniciales[6];
+
+  for (int i = 0; i < 6; i++)
+    iniciales[i] = posibles[rand()%14];
+
+  clock_t tini = clock();
+  Cifras(obj,iniciales);
+  clock_t tfin = clock();
+
+  cout << (tfin-tini)/(double)CLOCKS_PER_SEC << endl;
+}
