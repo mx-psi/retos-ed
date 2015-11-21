6c00f74d9cf0e825aab4fd90b2f5f40e59f4196d
josemam <jmlosvillares@correo.ugr.es>
2015-11-21 19:30:38 +0100

    If you change tabs to spaces you will be killed

--- Reto3/Pila.h
+++ Reto3/Pila.h
@@ -12,99 +12,99 @@
 #include <list>
 
 /**
- *  @brief T.D.A. Pila
- *
- *
- * Una instancia @e p del tipo de dato abstracto Pila_max sobre un dominio @e T es
- * una sucesión finita de elementos del mismo con un funcionamiento @e LIFO
- * (Last In, First Out}). En una pila, las operaciones de inserción, borrado
- * y consulta tienen lugar en uno de los extremos, denominado @e tope de la pila.
- * Una pila longitud @e n la denotamos
- *
- * - <a1,a2,a3,..,an>
- *
- * Tendremos acceso únicamente al elemento del @e Tope,
- * es decir, a @e an, y sólo podremos borrar, insertar o consultar esa posición.
- *
- * Si n=0 diremos que la pila está vacía.
- *
- * El espacio requerido para el almacenamiento es O(n), donde n es el número
- * de elementos de la pila.
- *
- * @example PruebaPila.cpp
-*/
+  *  @brief T.D.A. Pila
+  *
+  *
+  * Una instancia @e p del tipo de dato abstracto Pila_max sobre un dominio @e T es
+  * una sucesión finita de elementos del mismo con un funcionamiento @e LIFO
+  * (Last In, First Out}). En una pila, las operaciones de inserción, borrado
+  * y consulta tienen lugar en uno de los extremos, denominado @e tope de la pila.
+  * Una pila longitud @e n la denotamos
+  *
+  * - <a1,a2,a3,..,an>
+  *
+  * Tendremos acceso únicamente al elemento del @e Tope,
+  * es decir, a @e an, y sólo podremos borrar, insertar o consultar esa posición.
+  *
+  * Si n=0 diremos que la pila está vacía.
+  *
+  * El espacio requerido para el almacenamiento es O(n), donde n es el número
+  * de elementos de la pila.
+  *
+  * @example PruebaPila.cpp
+  */
 
 template<class T>
 class Pila {
 private:
-	std::list<T> lista; /**< Elementos de la pila */
+  std::list<T> lista; /**< Elementos de la pila */
 
 public:
-	/**
-		* @brief Constructor por defecto.
-		*/
-	Pila() {};
+/**
+  * @brief Constructor por defecto.
+  */
+  Pila() {};
 
-	/**
-		* @brief Constructor de copia.
-		*/
-	Pila(const Pila& p) {
-		lista = p.lista;
-	}
+  /**
+    * @brief Constructor de copia.
+    */
+  Pila(const Pila& p) {
+    lista = p.lista;
+  }
 
-	/**
-		* @brief Operador de asignación.
-		*/
-	Pila& operator=(const Pila& p) {
-		lista = p.lista;
-	}
+  /**
+    * @brief Operador de asignación.
+    */
+  Pila& operator=(const Pila& p) {
+    lista = p.lista;
+  }
 
-	/**
-		* @brief Indica si la pila está vacía.
-		* @return Si la pila está vacía
-		*/
-	bool vacia() const {
-		return lista.empty();
-	}
+  /**
+    * @brief Indica si la pila está vacía.
+    * @return Si la pila está vacía
+    */
+  bool vacia() const {
+    return lista.empty();
+  }
 
-	/**
-		* @brief Devuelve el elemento en el tope (versión constante).
-		* @return Elemento del tope
-		*/
-	const T& tope() const {
-		return lista.front();
-	}
+  /**
+    * @brief Devuelve el elemento en el tope (versión constante).
+    * @return Elemento del tope
+    */
+  const T& tope() const {
+    return lista.front();
+  }
 
-	/**
-		* @brief Devuelve el elemento en el tope (versión no constante).
-		* @return Elemento del tope
-		*/
-	T& tope() {
-		return lista.front();
-	}
+  /**
+    * @brief Devuelve el elemento en el tope (versión no constante).
+    * @return Elemento del tope
+    */
+  T& tope() {
+    return lista.front();
+  }
 
-	/**
-		* @brief Pone un elemento en el tope.
-		* @param t Elemento a poner
-		*/
-	void poner(const T& t) {
-		lista.push_front(t);
-	}
+  /**
+    * @brief Pone un elemento en el tope.
+    * @param t Elemento a poner
+    */
+  void poner(const T& t) {
+    lista.push_front(t);
+  }
 
-	/**
-		* @brief Elimina el elemento en el tope.
-		*/
-	void quitar() {
-		lista.pop_front();
-	}
+  /**
+    * @brief Elimina el elemento en el tope.
+    */
+  void quitar() {
+    lista.pop_front();
+  }
 
-	/**
-		* @brief Obtiene el tamaño de la pila.
-		* @return Tamaño de la pila
-		*/
-	int elementos() const {
-		return lista.size();	// A partir de C++11 es O(1). Antes, O(n)
-	}
+  /**
+    * @brief Obtiene el tamaño de la pila.
+    * @return Tamaño de la pila
+    */
+  int elementos() const {
+    return lista.size();	// A partir de C++11 es O(1). Antes, O(n)
+  }
 };
 
 #endif
